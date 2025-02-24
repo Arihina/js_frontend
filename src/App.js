9a618d7a94a2515a143b89be01b95dc9c13cd6e7
Alena <alena.kozlova2004@mail.ru>
2025-02-24 19:16:34 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 
+import ToDoTask from './ToDoTask';
+
 class App extends React.Component {
   render() {
     const data = [
       { "_id": "67b74eee9d931865e9f746d2", "name": "task 1", "discription": "None", "done": false },
-      { "_id": "67b74f2d9d931865e9f746d3", "name": "task 2", "discription": "None", "done": false },
+      { "_id": "67b74f2d9d931865e9f746d3", "name": "task 2", "discription": "None", "done": true },
       { "_id": "67b750ba9b7a7dc53cb0a379", "name": "task 3", "discription": "None", "done": false },
-      { "_id": "67b7544c3ebc5b32e5f26c44", "name": "task 4", "discription": "None", "done": false }
+      { "_id": "67b7544c3ebc5b32e5f26c44", "name": "task 4", "discription": "None", "done": true }
     ];
 
     function mapToDoList(task) {
       return (
-        <li key={task._id}>{task.name}</li>
+        <ToDoTask task={task} key={task._id} />
       )
     }
 
